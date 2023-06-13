509962d3ec28de0389f925fe44473efe631db71d
gusgusz <gustavoguimaraescamps@gmail.com>
2023-06-13 17:40:56 -0300

    initial Page

---
+++ projeto22-autoral-front-end/src/components/Options.tsx
@@ -0,0 +1,22 @@
+
+function Options() {
+
+    return (
+      <>
+   
+      <h3 className='hover:cursor-pointer hover:text-indigo-800'>Home</h3> 
+      <h3 className='hover:cursor-pointer hover:text-indigo-800'>Quem somos</h3> 
+      <h3 className='hover:cursor-pointer hover:text-indigo-800'>Produtos</h3> 
+      <h3 className='hover:cursor-pointer hover:text-indigo-800'>Avaliações</h3> 
+      <h3 className='hover:cursor-pointer hover:text-indigo-800'>Material para Download</h3> 
+      <h3 className='hover:cursor-pointer hover:text-indigo-800'>Contato</h3> 
+ 
+  
+    
+  
+      </>
+    )
+  }
+  
+  export default Options
+  
